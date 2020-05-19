3d9214f33c2f4c66e57065a9fd2fd993ac6314fc
italosilva01 <italo.silvan87@gmail.com>
2020-05-19 18:44:32 -0300

    enabling and disabling the textarea tag

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,16 +1,32 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import * as S from './S-Note';
 export default function Note (props){
 
-    const [content, setContent]= useState();
+    const [content, setContent]= useState(' ');
+    const [condition,setCondition] = useState(true);
+    
 
-    function convert(){
-        //convert the text from the textarea tag to markdown
-        const text = document.querySelector('textarea').value;
-        setContent(text);
-    }
+     useEffect(()=>{
+            //change the div's z-index attribute
+            console.log(`start Condition = ${condition}`);
+            const markdown = document.querySelector('.wrap>div');
+            const textarea = document.querySelector('textarea');
+            
+            if(condition){
+                //if textarea is disabled
+                markdown.classList.add('visible');
+                textarea.classList.add('hidden');
+                
+            }else{
+                markdown.classList.remove('visible');
+                textarea.classList.remove('hidden');
+
+            }
+
+        },[condition]);
+
 
     function userButtonTab(button){
         const textArea = document.querySelector('textarea')
@@ -33,19 +49,38 @@ export default function Note (props){
         //
     }
 
+    function saveContent(){
+        console.log('clicou');
+
+        //saves the content of the textarea tag
+        const text = document.querySelector('textarea').value;
+        setContent(text);
+        
+        //changes conditio's value
+        setCondition(!condition);
+    }
+
     return(
         <S.Paper>
             <div className="date">
                 <p>DAte teste</p> 
                 <div>
-                    <input id="editar" type='checkbox' onClick={convert}></input> 
+                    <input id="editar" type='checkbox' onClick={()=>saveContent()}></input> 
                     <label htmlFor="editar" >Editar</label>
                 </div>
             </div>
-           <textarea name="" id="" cols="30" rows="10" onKeyDown={(e)=>userButtonTab(e)} >
-           </textarea>
-           
-            <ReactMarkdown source={content}/>
+
+            <div className="wrap">
+                
+                <S.Markdown>
+                    <ReactMarkdown source={content}/>
+                </S.Markdown>
+                
+                <textarea name="" disabled={condition} id="" cols="30" rows="10" onKeyDown={(e)=>userButtonTab(e)} >
+                </textarea> 
+                
+            </div>
+
         </S.Paper>
     );
 }
--- src/components/Note/S-Note.js
+++ src/components/Note/S-Note.js
@@ -7,26 +7,72 @@ export const Paper = styled.div`
     position:relative;
     margin:auto;
     width:90%;
-    height:100%;
+    height:50vh;
     box-shadow: 0px 0px 10px rgba(0,0,0,0.2);
-
+    
+    .visible{
+        z-index:1 !important;
+        visibility:visible;
+    }
+    
+    .hidden{
+        z-index:100000 !important;
+        display:none;
+    }
+   
     .date{
         display:flex;
         justify-content:space-between;
         width:100%;
     }
     
+    .wrap{
+        position:relative;
+        display:grid;
+        grid-template-areas:
+        'a a a'
+        'a a a'
+        'a a a';
+
+        width:100%;
+        align-items:center;
+
+    }
 
     textarea{
-        width:90%;
+        position:relative;
+        width:100%;
+        background-color:rgba(0,0,0,0.2);
         font-size:1.2rem;
         border:none;
+        z-index:3;
+        grid-area:a;
 
     }
     @media (min-width:800px){
         width:60%;
+        
+        .wrap{
+        }
+        textarea{
+            width:90%;
+        }
     }
     @media (min-width:1000px){
         width:800px;
     }
+`;
+
+export const Markdown = styled.div`
+    position:relative;
+    width:100%;
+    border:none;
+    font-size:1.2rem;
+    background-color:pink;
+    z-index:2;
+    grid-area:a;
+
+
+    visibility:hidden;
+
 `;
